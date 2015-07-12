504fcaa919cf4913d7b1389052db84e61a733677
Admin <admin@Mac-Admin.local>
2015-07-12 18:16:54 +0300

    Initial Commit

---
+++ Triangle/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Triangle
+//
+//  Created by Admin on 12.07.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
