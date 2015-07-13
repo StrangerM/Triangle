215c69f519db12bd1a7e7812e4a9cccec49193fa
Admin <admin@Mac-Admin.local>
2015-07-13 19:48:32 +0300

    22

--- Triangle/Triangle.m
+++ Triangle/Triangle.m
@@ -27,14 +27,23 @@
     
     
     int sqr;
-    sqr = ((3 - 1) * (5 - 1) - (3 - 1) * (1 - 1))/2;
+    sqr = (3 - 1) * (5 - 1) - (3 - 1) * (1 - 1);
     return sqr;
     
     }
 - (int)checkpoint
 {
-
-    
+//    x1=1 y1=1
+//    x2 =3 y2=4
+//    x3=5 y3=1
+//    x4 y4
+//    sqr0 = (3 - 1) * (5 - 1) - (3 - 1) * (1 - 1);
+//    sqr1 =(3-1)*(x4-1)-(3-1)*(y4-1)
+//    sqr2=(y4-1)*(5-1)-(x4-1)*(1-1)
+//    sqr3 =(3-y4)*(5-x4)-(3-x4)*(1-y4)
+//    sqr=sqr1+sqr2+sqr3
+//    if (sqr-sqr0<=0 NSlog (@"point in")
+//        else NSLOG(@"point out")
     
     
     return <#expression#>
